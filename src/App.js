50616139787f913b6ca6da99aafb18abbf53f86c
marukuma920 <marukuma920@gmail.com>
2020-04-28 16:02:21 +0900

    create hogehoge

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 
-import React,{Component} from 'react';
+import React from 'react';
 
-class App extends Component{
-  render(){
-    return <div><h1>hellod</h1></div>;
-  }
+//class App extends Component{
+//  render(){
+//    return <div><h1>hellod</h1></div>;
+//  }
+//}
+//
+
+const App = () =>{
+  return <div>
+    <Cat/>
+    <Cat/>
+    <Cat/>
+    </div>
 }
 
+const Cat = () =>{
+  return <div>Meow!</div>
+}
 export default App;
