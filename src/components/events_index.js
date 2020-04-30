1e34b96bad93ff32b3539e845cc1ba72776351ce
marukuma920 <marukuma920@gmail.com>
2020-04-30 15:18:19 +0900

    inplent event new component

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import {readEvents} from '../actions'
 
 import _ from 'lodash'
+import {Link} from 'react-router-dom'
 
 class EventsIndex extends Component {
   componentDidMount(){
@@ -14,35 +15,33 @@ class EventsIndex extends Component {
 
   renderEvents(){
     return _.map(this.props.events, event =>(
-    <tr key={event.id}>
-      <td>{event.id}</td>
-      <td>{event.title}</td>
-      <td>{event.body}</td>
-    </tr>  
-  ))
-    
-  }
-
+        <tr key={event.id}>
+          <td>{event.id}</td>
+          <td>{event.title}</td>
+          <td>{event.body}</td>
+        </tr>  
+      )
+    )
+ }
 
   render(){
-
     return (
-    <React.Fragment>
-    <table>
-      <thead>
-        <tr >
-          <th>id</th>
-          <th>Title</th>
-          <th>Body</th>
-        </tr>
-      </thead>
-      <tbody>
-        {this.renderEvents()}
-      </tbody>
-    </table>
-       </React.Fragment>
+      <React.Fragment>
+        <table>
+          <thead>
+            <tr >
+              <th>id</th>
+              <th>Title</th>
+              <th>Body</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.renderEvents()}
+          </tbody>
+        </table>
+        <Link to='/events/new'>New Event</Link>
+      </React.Fragment>
     )
-
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createStore, applyMiddleware} from 'redux'
 import { Provider} from 'react-redux'
+import { BrowserRouter, Route, Switch} from 'react-router-dom'
 
 import thunk from 'redux-thunk'
 import './index.css';
 import reducer from './reducers'
 
 import EventsIndex from './components/events_index';
+import EventsNew from './components/events_new';
 import * as serviceWorker from './serviceWorker';
 
 
@@ -19,8 +21,12 @@ const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
   <Provider store={store}>
- 
-    <EventsIndex />
+    <BrowserRouter>
+      <Switch>
+         <Route exact path="/events/new" component={EventsNew} />
+         <Route exact path="/" component={EventsIndex} />
+      </Switch>
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
